5ffa2cf90ae5e348606915b6aa119bc49a136415
Macflash <alex.brofo@outlook.com>
2015-09-20 23:28:47 -0700

    Basic angular app

---
+++ PRAX/App/app.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="typescript/person.ts" />
+/// <reference path="angular/angular.d.ts" />
+
+var PRAX = angular.module("PRAX", []);
+
+PRAX.controller("peopleController", function ($scope) {
+    $scope.people = [];
+    $scope.people.push(new Person("tom"));
+    $scope.people.push(new Person("george"));
+});
+
+PRAX.directive("personDetail", function () {
+    return {
+        restrict: "A",
+        templateUrl: "templates/persondetail.html"
+    };
+});
+
+PRAX.controller("personDetailController", function ($scope) {
+    $scope.expanded = false;
+});
---
+++ PRAX/App/typescript/person.ts
@@ -0,0 +1,79 @@
+﻿class Need {
+    name: string;
+    rate: number;
+    value: number;
+    constructor(nane: string, rate: number) {
+        this.name = name;
+        this.rate = rate;
+        this.value = 0;
+    }
+    update(time: number) {
+        if (this.value >= 100) {
+            this.value = 100;
+        }
+        else {
+            this.value += this.rate * time;
+        }
+    }
+    satiate(amount?: number) {
+        if (amount == null) {
+            this.value = 0;
+        }
+        else {
+            this.value -= amount;
+            if (this.value < 0) {
+                this.value = 0;
+            }
+        }
+    }
+}
+
+class Shift {
+    name: string;
+    rate: number;
+    progress: number;
+    constructor(name: string, rate: number) {
+        this.name = name;
+        this.rate = rate;
+    }
+    update(time: number) {
+        this.progress += this.rate * time;
+        if (this.progress >= 100) {
+            return true;
+        }
+        return false;
+    }
+}
+
+interface NeedsArray {
+    [index: number]: Need;
+}
+
+class Person {
+    name: string;
+    working: boolean;
+    needs: NeedsArray;
+    task: Shift;
+
+    constructor(name: string) {
+        this.name = name;
+        this.working = false;
+    }
+
+    update(time: number) {
+        // increment needs
+        for (var need in this.needs) {
+            need.update(time);
+        }
+
+        // check if working, if so do that
+        if (this.working) {
+
+        }
+        
+        // otherwise figure out what you should satisfy
+        else {
+
+        }
+    }
+}
